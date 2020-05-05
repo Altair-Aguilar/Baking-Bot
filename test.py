34b1d926a746c19e600f07dd54938395f5103d20
Altair-Aguilar <45566776+Altair-Aguilar@users.noreply.github.com>
2020-05-05 12:13:51 -0400

    Update test.py
    Imported required modules. set a prefix (temporary), added a string to be sent when bot is ready. Added the say command which will cause him to repeat any word as many times as you specify.

--- test.py
+++ test.py
@@ -1,1 +1,18 @@
-{\rtf1}
+from discord import *
+from discord.ext import commands
+import requests
+
+#Setting the prefix that must be used before each command
+client = commands.Bot(command_prefix = '!')
+
+#When bot is ready to be used, Bot is active will be printed, and commands will be ready to be used.
+@client.event
+async def on_ready():
+	print("Bot is active.")
+
+#When run, he will repeat what you say as many times as you specify (default of 1).
+@client.command()
+async def say(ctx, word, times=1):
+	for x in range(times):
+		await ctx.send(str(word))
+
