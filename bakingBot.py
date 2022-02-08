a1f2cb680cc5c649154c7b52dbe27f6344abd21c
Anthony Skinner <45566776+AnthonySkinner@users.noreply.github.com>
2022-02-08 09:37:24 -0500

    Add the helpcommand command
    Adds a command that sends a help message about each command, drawing this message from a dictionary

--- bakingBot.py
+++ bakingBot.py
@@ -16,7 +16,9 @@ async def on_ready():
 async def say(ctx, word, times=1):
 	for x in range(times):
 		await ctx.send(str(word))
-#return recipe based on arguments given by user
+
+
+#return recipe based on arguments given by user NEED TO RENAME TEST VARIABLES!!!!
 @client.command()
 async def recipe(ctx, *args):
 	x = ""
@@ -29,5 +31,14 @@ async def recipe(ctx, *args):
 	print(argus)
 	send = Recipe(argus[0], argus[1], argus[2], argus[3], argus[4], argus[5])
 	await ctx.send(str(send.recipe))
+
+
+
+#Help Command, call the help command by itself for a list of commands to get help on, or with the command name for specific help on that command
+help_list = {"recipe" : "To use this command, call .recipe with 6 variables: hydration percentage, salt percentage, yeast percentage, sugar percentage, oil percentage, and the total amount of dough."}
+
+@client.command()
+async def helpcommand(ctx, command):
+	await ctx.send(help_list[command.lower()])
 
 client.run('NzA3MjY1MDUzOTEyNTk2NjQy.XrGTNA.OzqqScKKvafRma6gJAP9m45ehUQ')
